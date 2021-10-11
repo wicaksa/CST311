e63f869044ab0db55e97ffce1dca0907cdc1c351
wicaksa <50036161+wicaksa@users.noreply.github.com>
2021-10-10 21:11:12 -0700

    Update Client.py
    Added more comments to the code and cleaned it up.

--- ProgrammingAssignment3/Client.py
+++ ProgrammingAssignment3/Client.py
@@ -1,61 +1,61 @@
 # Client.py
 
-# Group: Cheuk On-Yim, Matthew Stoney, Paola Torres, Wicaksa Munajat
+# Group: Cheuk On Yim, Matthew Stoney, Paola Torres, Wicaksa Munajat
 # Date: 10/07/2021
 # Title: Client.py
 # Programming Assignment 3 
 # CST311 CSUMB CS ONLINE FALL 2021
 
-# Description: This is a TCP Client program that will connect to a client. 
-# It will send a message inputted by the user to the client. 
+# Description: This is a TCP Client program that will connect to a server. 
+# It will send a message inputted by the user to the server. 
 
 from socket import *
 import sys
 import threading
 
-global clientName, received
-
-# connect with server at port
+# This function creates a clientSocket and returns it. 
+# Input : serverName : String
+#         serverPort : int
+# Output: socket object 
 def getConnection(serverName, serverPort):
     clientSocket = socket(AF_INET, SOCK_STREAM)
     clientSocket.connect((serverName, serverPort))
     return clientSocket
 
-# method to send message to server
+# This function sends message to connected server.
+# Input : clientSocket : socket object
+# Output: None
 def sendMessages(clientSocket):
     global clientMessage
     clientMessage = input('Enter Message to send to Server: ')
     clientSocket.send(clientMessage.encode())
 
-# receive message from server
+# This function receives messages from the server and prints it out. 
+# Input : clientSocket : socket object
+# Output: None
 def receiveMessages(clientSocket):
     serverMessage = clientSocket.recv(1024)
     print(serverMessage.decode())
 
-
+# This is the main function.
 def main():
-    # Get server name and server port
+    # Get server name and server port.
     serverName = gethostbyname(gethostname())
     serverPort = serverPort = 12000
 
-    # Get Client Socket
+    # Get Client Socket.
     clientSocket = getConnection(serverName,serverPort)
-
-    ## Start your thread
-    ##t.start()
 
     # Receive message when you connect to server.
     receiveMessages(clientSocket)
 
-    # Send message to server
+    # Send message to server inputted by the user.
     sendMessages(clientSocket)
 
-    # Receive Message back from the server after both connections.
+    # Receive Message back from the server after both connections have been made.
     receiveMessages(clientSocket)
 
-    # Close socket
-    # clientSocket.close()
-
+# Call to main
 if __name__ == "__main__":
     main()
     
